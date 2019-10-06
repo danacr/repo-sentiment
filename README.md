735d3a9c5b1580ebf2c118d7c4615effaff70ab7
danacr <dan@acristinii.com>
2019-10-06 16:05:36 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# repo-sentiment
+# Analyze user comments sentiment
+
+Requires Google Cloud Natural Language API: https://cloud.google.com/natural-language/docs/quickstart-client-libraries
+
+Usage:
+
+```
+export GOOGLE_APPLICATION_CREDENTIALS="/home/user/Downloads/[FILE_NAME].json"
+node index.js GITHUB_API_KEY GITHUB_USERNAME_TO_CHECK
+```
---
+++ index.js
@@ -0,0 +1,73 @@
+// Imports the Google Cloud client library
+const language = require("@google-cloud/language");
+
+const { graphql } = require("@octokit/graphql");
+const { createAppAuth } = require("@octokit/auth-app");
+
+// Detects the sentiment of the document
+
+async function sentiment(text) {
+  // Creates a client
+  const client = new language.LanguageServiceClient();
+  // Prepares a document, representing the provided text
+  const document = {
+    content: text,
+    type: "PLAIN_TEXT"
+  };
+  //   console.log(text);
+  const [result] = await client.analyzeSentiment({ document });
+
+  const sentiment = result.documentSentiment;
+  //   console.log(`Document sentiment:`);
+
+  const sentences = result.sentences;
+  //   sentences.forEach(sentence => {
+  // console.log(`Sentence: ${sentence.text.content}`);
+  // console.log(`  Score: ${sentence.sentiment.score}`);
+  // console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
+  //   });
+  return [sentiment.score, sentiment.magnitude];
+}
+
+async function userComments(user, num) {
+  const comments = graphql({
+    query: `query user($login: String!, $num: Int!) {
+            user(login: $login) {
+              issueComments(last: $num) {
+                nodes {
+                  body
+                }
+              }
+            }
+          }`,
+    login: user,
+    num: num,
+    headers: {
+      authorization: `token ` + process.argv[2]
+    }
+  });
+  return comments;
+}
+
+async function average(user) {
+  res = await userComments(user, 100);
+  var sentsum = 0;
+  var sentcount = 0;
+  for (let x of res.user.issueComments.nodes) {
+    [sent, magn] = await sentiment(x.body);
+    if (magn >= 2) {
+      sentsum = sentsum + parseFloat(sent);
+      sentcount++;
+    }
+  }
+  console.log(
+    "Sentiment sum: ",
+    sentsum,
+    "Number of comments with mgn >=2 : ",
+    sentcount,
+    "Sentiment average: ",
+    sentsum / sentcount
+  );
+  return [sentsum, sentcount, sentsum / sentcount];
+}
+average(process.argv[3]);
